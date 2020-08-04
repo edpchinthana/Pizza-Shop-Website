bdfa8d05093c862bbd21ca5dcd0ae55ee7ce7701
edpchinthana <edpchinthana@outlook.com>
2020-08-05 03:23:22 +0530

    slideshow function

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 var slides = new Array();
-var index = 0;
+var index = 1;
 function creatingSlides() {
     console.log('creating slides');
-    let slideImages = ['slide2.jpg','slide2.jpg','slide3.jpg'];
+    let slideImages = ['slide2.jpg','slide3.jpg','slide2.jpg'];
 
 
     for(let x=0;x<slideImages.length;x++){
@@ -10,13 +10,31 @@ function creatingSlides() {
         slide.src = "./res/images/"+slideImages[x];
         slide.classList.add('slide');
         slides.push(slide);
+        document.getElementById("slider").appendChild(slide);
     }
-    
+    document.getElementById("slider").style.left = "-100vw"
 }
 
 function showSlide(e){
-    slide
+
+    if(e==1){
+        if(index==slides.length-1){
+            document.getElementById("slider").style.left = "0vw"
+            index=0;
+        }else{
+            document.getElementById("slider").style.left = ((index+1)*(-100))+'vw'
+            index++;
+        }
+    }else{
+        if(index==0){
+            document.getElementById("slider").style.left = ((index+slides.length-1)*(-100))+'vw'
+            index=slides.length-1;
+        }else{
+            document.getElementById("slider").style.left = ((index-1)*(-100))+'vw'
+            index--;
+        }
+    }
 }
 
+creatingSlides();
 
-creatingSlides()
